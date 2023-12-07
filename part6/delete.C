baf40610b688eae285919cb057c44091e638bf97
akjoshi2 <akjoshi2003@gmail.com>
2023-12-06 20:07:11 -0600

    Implement delete.C

--- part6/delete.C
+++ part6/delete.C
@@ -16,11 +16,43 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
+	// return error if any input is null
+    if (relation == "" || attrValue == nullptr) {
+        return BADSCANPARM; // idk if this is the right error message
+    }
 
+    Status status;
+    RID rid;
+    Record rec;
+    AttrDesc attrDesc;
 
+    HeapFileScan hfs(relation, status);
+    if (status != OK) { 
+        return status;
+    }
 
+    //  get the attribute description
+    status = attrCat->getInfo(relation, attrName, attrDesc);
+    if (status != OK) { 
+        return status; 
+    }
+
+    // scan through the heapfile with matching type, attrValue, and op
+    status = hfs.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, attrValue, op);
+    if (status != OK) return status;
+
+    // delete tuples
+    while(hfs.scanNext(rid) == OK) {
+        status = hfs.deleteRecord();
+        if (status != OK) {
+            return status;
+        }
+    }
+
+    // close the scan
+    hfs.endScan();
+
+    return OK;
 }
 
 
